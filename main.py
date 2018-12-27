1081691abf657e4cbc2f6dc9aad4743e77431470
w1624401 <34352629+w1624401@users.noreply.github.com>
2018-12-27 21:29:36 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from lexering import Lexer
+from parsering import Pars
+
+text_input = "(4 + 4 - 2);"
+
+lexer = Lexer().get_lexer()
+tokens = lexer.lex(text_input)
+
+pg = Pars()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens).eval()
